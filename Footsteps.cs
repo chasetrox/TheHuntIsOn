420d567004175e22fa3fb4bb96ec4234d7e8a7be
Alex Pantuck <apantuck@gmail.com>
2018-04-16 21:10:06 -0400

    Additional functionality

--- Footsteps.cs
+++ Footsteps.cs
@@ -1,26 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 /* Alex Pantuck
  * Comp50
+ * 4/16/18
+ * Footsteps.cs
  * 
  * This script places footsteps down on any surface whose layer is "Ground"
  * Attach this script to the player, provide the transform of a gameobject at the player's feet, and provide
- * the prefab to be instantiated.
+ * the prefab to be instantiated. Also provide SFX for footsteps.
+ * Also, to make water footsteps work, add a trigger collider to water and give it the tag "water"
+ * Also, in edit->project settings->input make sure there is a button called "walk" I recommend shift as its button.
+ * 
+ * Sounds and prefab instantiation is determined by a displacement, so these are not networked.
+ * What IS networked, however, is the "walk" state--if a player is holding the walk button,
+ * they move slower, but provide no footprints or footstep sounds
  */
 
-public class Footsteps : MonoBehaviour
+[RequireComponent(typeof(AudioSource))]
+public class Footsteps : NetworkBehaviour
 {
 
-    public GameObject footstepPrefab;
-    public Transform footPos;
-    public float footDistance = 1;
-    public float groundCheckDist = 0.2f;
+    public GameObject footstepPrefab; // Prefab of footprint sprites
+    public Transform footPos; // Position of player feet, where to check ground from
+    public float footDistance = 1; // Minimum displacement to place a footprint and play sound
+    public AudioClip[] snowSFX; // Sounds of walking in snow
+    public AudioClip[] waterSFX; // Sounds of walking in water
+   
+    [SyncVar] private bool isWalking = false;
+    [SyncVar] private bool inWater = false;
+    private float groundCheckDist = 0.2f;
     private Vector3 lastPos;
     private float displacement;
     private Vector3 down;
-    private int mask;
+    private int mask; // Layer mask for ground
 
     private void Start()
     {
@@ -31,6 +44,12 @@ public class Footsteps : MonoBehaviour
 
     void Update ()
     {
+        if (isLocalPlayer)
+            isWalking = Input.GetButton("walk");
+
+        if (isWalking)
+            return;
+
         // Displacement from last footprint
         displacement = Vector3.Distance(lastPos, transform.position);
 
@@ -38,7 +57,10 @@ public class Footsteps : MonoBehaviour
         // a new footprint
         if (displacement > footDistance)
         {
+            // Update last known position
             lastPos = transform.position;
+            // Play footstep sound
+            playSound();
 
             // Raycast down from player feet, check if it hits ground
             RaycastHit hit;
@@ -56,4 +78,30 @@ public class Footsteps : MonoBehaviour
         
 	}
 
+    // Play a random sfx from appropriate array of sfx
+    private void playSound()
+    {
+        if (inWater && waterSFX.Length > 0)
+        {
+            int index = Random.Range(0, waterSFX.Length);
+            GetComponent<AudioSource>().PlayOneShot(waterSFX[index]);
+        }
+        else if (snowSFX.Length > 0)
+        {
+            int index = Random.Range(0, snowSFX.Length);
+            GetComponent<AudioSource>().PlayOneShot(snowSFX[index]);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("water"))
+            inWater = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("water"))
+            inWater = false;
+    }
 }
