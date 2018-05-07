02b8b6f88d6fdfe1cad20865ad8e05e6c9ea4de1
chasetrox <chasetrox@gmail.com>
2018-05-07 12:44:56 -0400

    Added menu

---
+++ Assets/LoadGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class LoadGame : MonoBehaviour 
+{	
+    public void NextLevelButton()
+    {
+        Application.LoadLevel("ForestLevel_B");
+    }
+}
