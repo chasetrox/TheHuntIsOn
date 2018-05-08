f833db8127a283abd55fb667a2dda59f163e02a3
chasetrox <chasetrox@gmail.com>
2018-05-07 21:26:31 -0400

    hunter changes

--- Assets/Scripts/OverrideNetworkManager.cs
+++ Assets/Scripts/OverrideNetworkManager.cs
@@ -13,9 +13,9 @@ class OverrideNetworkManager : NetworkManager
     	Transform playerSpawnPos = GetStartPosition();
     	GameObject player;
     	if (numPlayers == 0)
-        	player = GameObject.Instantiate(beastPrefab, playerSpawnPos.position, Quaternion.identity);
-    	else if (numPlayers == 1)
         	player = GameObject.Instantiate(hunterPrefab, playerSpawnPos.position, Quaternion.identity);
+    	else if (numPlayers == 1)
+        	player = GameObject.Instantiate(beastPrefab, playerSpawnPos.position, Quaternion.identity);
         else {
         	player = null;
         	Debug.Log("There's more than 2 players in the game! Not spawning anyone!!");
